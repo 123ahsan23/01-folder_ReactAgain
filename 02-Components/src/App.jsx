e8211f6eccca5c2747dc17f3205ea90c62afe27e
Syed Ahsan Ali <ahsankazmi2002@gmail.com>
2026-03-31 13:10:32 +0500

    02-Components done

---
+++ 02-Components/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from './Components/card'
+import NavBar from './Components/NavBar'
+
+const App = () => {
+  return (
+    <div>
+      
+     <NavBar />
+     <Card />
+     
+    </div>
+  )
+}
+
+export default App

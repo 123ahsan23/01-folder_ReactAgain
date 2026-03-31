ede5c7167502915d20a0a3f77658b6daacd00ad2
Syed Ahsan Ali <ahsankazmi2002@gmail.com>
2026-03-31 10:59:53 +0500

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,18 @@
+const App = ()=>{
+
+  return (
+    <>
+    {/* heading */}
+    <div>
+      <h1>Hello h1 </h1>
+      <h2>Hello h2</h2>
+    </div>
+
+    {/* Paragraph */}
+    <div><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, natus beatae rem quis sit placeat, ea cupiditate id consequuntur magnam tempora voluptatibus nemo veniam nesciunt quo quasi amet, laborum quia. Tempora quasi molestias, omnis ratione possimus minima, voluptate sint nemo deleniti natus nostrum consequatur placeat!</p></div>
+
+    </>
+  )
+}
+
+export default App;
